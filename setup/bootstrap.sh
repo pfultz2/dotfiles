bbf3a738ca1f198d7ac8daf89138c1a705cfc7a7
Paul <pfultz2@yahoo.com>
2016-01-29 10:23:37 -0800

    Better support for mingw

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -5,6 +5,8 @@ export DOTFILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )
 echo "Add to bash profile: source $DOTFILES/etc/bash/config"
 if [ "$(uname)" == "Darwin" ]; then
     echo "source $DOTFILES/etc/bash/config" >> ~/.bash_profile
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+	echo "source $DOTFILES/etc/bash/config" >> ~/.bash_profile
 else
     echo "source $DOTFILES/etc/bash/config" >> ~/.bashrc
 fi
