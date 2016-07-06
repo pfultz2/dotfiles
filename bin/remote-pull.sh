fec2352d124df6f791422e195eb43cd33cb22e08
Paul <pfultz2@yahoo.com>
2016-07-05 20:38:37 -0500

    Add options to remote pull

--- bin/remote-pull.sh
+++ bin/remote-pull.sh
@@ -2,9 +2,24 @@
 
 source $DOTFILES/bin/remote-setup.sh
 
-rsync --verbose --recursive --times --compress --progress\
-    --exclude ".hg" \
-    --exclude ".git" \
-    --exclude "*build" \
-    --exclude "build*" \
-        $HOST:$DEST_DIR/ $CURR_DIR
+RSYNC_EXCLUDE_OPTS=(--exclude ".hg" --exclude ".git" --exclude "*build" --exclude "build*")
+
+for i in "$@"
+do
+case $i in
+    -a|--all)
+    RSYNC_EXCLUDE_OPTS=()
+    shift # past argument=value
+    ;;
+    -d|--delete)
+    RSYNC_OPTS+=(--delete --delete-excluded)
+    shift # past argument=value
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+echo "rsync --verbose --recursive --times --compress --progress ${RSYNC_OPTS[@]} ${RSYNC_EXCLUDE_OPTS[@]} $HOST:$DEST_DIR/ $CURR_DIR"
+rsync --verbose --recursive --times --compress --progress "${RSYNC_OPTS[@]}" "${RSYNC_EXCLUDE_OPTS[@]}" $HOST:$DEST_DIR/ $CURR_DIR
